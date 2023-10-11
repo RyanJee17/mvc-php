0dd65d2abcb31689893cb2db419dba91c479d700
RyanJee17 <ryan.jeewooth@gmail.com>
2023-10-11 17:28:10 +0200

    push de fin de tp

--- blog/index.php
+++ blog/index.php
@@ -1,3 +1,29 @@
+<?php
+   // Connexion à la base de données
+   try {
+      $database = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', 'root');
+   } catch (Exception $e) {
+      die('Erreur : ' . $e->getMessage());
+   }
+
+   // On récupère les 5 derniers billets
+   $statement = $database->query(
+      "SELECT id, titre, contenu, DATE_FORMAT(date_creation, '%d/%m/%Y à %Hh%imin%ss') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5"
+   );
+
+   $posts = [];
+   while ($row = $statement->fetch()) {
+      $post = [
+         'title' => $row['titre'],
+         'content' => $row['contenu'],
+         'frenchCreationDate' => $row['date_creation_fr'],
+      ];
+
+   }
+   $posts[] = $post;
+
+?>
+
 <!DOCTYPE html>
 <html>
    <head>
@@ -11,38 +37,26 @@
       <p>Derniers billets du blog :</p>
 
       <?php
-      // Connexion à la base de données
-      try
-      {
-          $bdd = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', 'root');
-      }
-      catch(Exception $e){
-            die( 'Erreur : '.$e->getMessage()   );
-      }
-
-      // On récupère les 5 derniers billets
-      $req = $bdd->query('SELECT id, titre, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5');
-
-      while ($donnees = $req->fetch())
-      {
+         foreach ($posts as $post) {
       ?>
-      <div class="news">
-         <h3>
-            <?php echo htmlspecialchars($donnees['titre']); ?>
-            <em>le <?php echo $donnees['date_creation_fr']; ?></em>
-         </h3>
-         <p>
+
+         <div class="news">
+            <h3>
+               <?php echo htmlspecialchars($post['title']); ?>
+               <em>le <?php echo $post['frenchCreationDate']; ?> </em>
+            </h3>
+            <p>
+               <?php
+                  // On affiche le contenu du billet
+                  echo nl2br(htmlspecialchars($post['content']));
+               ?>
+               <br />
+               <em><a href="#">Commentaires</a></em>
+            </p>
+         </div>
          <?php
-         // On affiche le contenu du billet
-                echo    nl2br ( htmlspecialchars( $donnees['contenu']));
-         ?>
-         <br />
-         <em><a href="#">Commentaires</a></em>
-         </p>
-      </div>
-      <?php
       } // Fin de la boucle des billets
-      $req->closeCursor();
+      $statement->closeCursor();
       ?>
    </body>
 </html>
