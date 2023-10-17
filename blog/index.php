43f87d7885f4ba92b54511f1ed6dc7a22e31b2d2
RyanJee17 <ryan.jeewooth@gmail.com>
2023-10-17 15:08:27 +0200

    Isolez l'affichage du traitement PHP

--- blog/index.php
+++ blog/index.php
@@ -1,62 +1,29 @@
 <?php
-   // Connexion à la base de données
+   
    try {
-      $database = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', 'root');
+      $database = new PDO(
+         'mysql:host=localhost;dbname=blog;charset=utf8',
+         'root',
+         'root'
+      );
    } catch (Exception $e) {
       die('Erreur : ' . $e->getMessage());
    }
 
-   // On récupère les 5 derniers billets
    $statement = $database->query(
-      "SELECT id, titre, contenu, DATE_FORMAT(date_creation, '%d/%m/%Y à %Hh%imin%ss') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5"
+      "SELECT id, titre, contenu, DATE_FORMAT(date_creation,
+   '%d/%m/%Y à %Hh%imin%ss') AS date_creation_fr
+   FROM billets ORDER BY date_creation DESC LIMIT 0, 5"
    );
-
    $posts = [];
-   while ($row = $statement->fetch()) {
+   while (($row = $statement->fetch())) {
       $post = [
          'title' => $row['titre'],
+         'french_creation_date' => $row['date_creation_fr'],
          'content' => $row['contenu'],
-         'frenchCreationDate' => $row['date_creation_fr'],
       ];
-
+      $posts[] = $post;
    }
-   $posts[] = $post;
 
+   require('../templates/homepage.php');
 ?>
-
-<!DOCTYPE html>
-<html>
-   <head>
-      <meta charset="utf-8" />
-      <title>Le blog de l'AVBN</title>
-      <link href="style.css" rel="stylesheet" />
-   </head>
-
-   <body>
-      <h1>Le super blog de l'AVBN !</h1>
-      <p>Derniers billets du blog :</p>
-
-      <?php
-         foreach ($posts as $post) {
-      ?>
-
-         <div class="news">
-            <h3>
-               <?php echo htmlspecialchars($post['title']); ?>
-               <em>le <?php echo $post['frenchCreationDate']; ?> </em>
-            </h3>
-            <p>
-               <?php
-                  // On affiche le contenu du billet
-                  echo nl2br(htmlspecialchars($post['content']));
-               ?>
-               <br />
-               <em><a href="#">Commentaires</a></em>
-            </p>
-         </div>
-         <?php
-      } // Fin de la boucle des billets
-      $statement->closeCursor();
-      ?>
-   </body>
-</html>
